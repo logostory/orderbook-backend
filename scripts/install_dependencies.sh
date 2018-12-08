13cede54e8721db619b5af13fdba4a5b2671851b
torypapa <torypapa.io@gmail.com>
2018-12-08 09:28:41 +0900

    update jdk install commend

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-sudo add-apt-repository ppa:openjdk-r/ppa
-sudo apt-get update
-sudo apt-get install openjdk-8-jdk
-sudo update-alternatives --config java
-sudo update-alternatives --config javac
+sudo apt install openjdk-8-jdk-headless
