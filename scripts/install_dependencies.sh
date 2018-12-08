0d33ca116d8869a5f94e56da542cd15148c7f5e0
torypapa <torypapa.io@gmail.com>
2018-12-08 09:17:33 +0900

    add install openjdk-8

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+sudo update-alternatives --config java
+sudo update-alternatives --config javac
