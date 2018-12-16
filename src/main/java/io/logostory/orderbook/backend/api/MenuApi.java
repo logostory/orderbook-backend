3029121a999e998657030ae19fb2769d81dfa88f
torypapa <torypapa.io@gmail.com>
2018-12-16 12:24:35 +0900

    add find menu

--- src/main/java/io/logostory/orderbook/backend/api/MenuApi.java
+++ src/main/java/io/logostory/orderbook/backend/api/MenuApi.java
@@ -4,6 +4,7 @@ import io.logostory.orderbook.backend.domain.dto.category.CategoryDto;
 import io.logostory.orderbook.backend.domain.dto.menu.MenuDto.*;
 import io.logostory.orderbook.backend.domain.entity.category.Category;
 import io.logostory.orderbook.backend.domain.entity.menu.Menu;
+import io.logostory.orderbook.backend.repository.MenuRepository;
 import io.logostory.orderbook.backend.service.MenuService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,8 @@ public class MenuApi {
 
 
     @Autowired  MenuService menuService;
+    @Autowired
+    MenuRepository menuRepository;
 
     @PostMapping(path = "/shops/{shopId}/menus")
     public ResponseEntity<List<MenuSearchResultDto>> addMenuList(
@@ -28,4 +31,16 @@ public class MenuApi {
         List<Menu> menus = menuService.addMenuList(shopId, dtos);
         return ResponseEntity.ok( menus.stream().map(m -> new MenuSearchResultDto(m)).collect(Collectors.toList()) );
     }
+
+
+    @GetMapping(path = "/shops/{shopId}/menus/{menuId}")
+    public ResponseEntity<MenuSearchResultDto> findMenu(@PathVariable Long shopId, @PathVariable Long menuId) {
+
+//        Menu menu =  menuRepository.findById(menuId).get();
+
+        Menu menu = menuRepository.findMenuByShopAndMenu(shopId, menuId);
+
+        return ResponseEntity.ok(new MenuSearchResultDto(menu));
+    }
+
 }
--- src/main/java/io/logostory/orderbook/backend/repository/MenuRepository.java
+++ src/main/java/io/logostory/orderbook/backend/repository/MenuRepository.java
@@ -2,7 +2,11 @@ package io.logostory.orderbook.backend.repository;
 
 import io.logostory.orderbook.backend.domain.entity.menu.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    @Query("select m from Menu m where m.shop.shopId = ?1 and m.menuId = ?2")
+    public Menu findMenuByShopAndMenu(Long shopId, Long menuId);
 
 }
